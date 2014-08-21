14cb668aec3e518c8812099229266bb2c6434cf5
Narendra747 <detectiven747@gmail.com>
2014-08-21 20:26:10 +0530

    Update 2wd.h

--- 2wd.h
+++ 2wd.h
@@ -1,27 +1,35 @@
 /*
-  2wd.h - Library for controlling robots with two wheel drive system.
+  Twd.h - Library for controlling robots with two wheel drive system.
   Created by Narendra Patwardhan, August, 2014.
-  Released into the public domain.
+  
+
+Released into the public domain.
 */
-#ifndef 2wd_h
-#define 2wd_h
+#ifndef Twd_h
+#define Twd_h
 
 #include "Arduino.h"
 
-class 2wd
+class Twd
 {
   public:
-    2wd(byte motor1logic1,byte motor1logic2,byte motor2logic1,byte motor2logic2);
+    Twd(byte 
+
+motor1logic1,byte motor1logic2,byte motor2logic1,byte motor2logic2);
     void moveForward();
     void moveBackward();
-    void turnRight();
+    void 
+
+turnRight();
     void turnLeft();
     void turnSharpright();
     void turnSharpleft();
     void applyBrakes();
     
   private:
-    byte motor1logic1,motor1logic2,motor2logic1,motor2logic2;
+    
+
+byte _motor1logic1,_motor1logic2,_motor2logic1,_motor2logic2;
 };
 
 #endif
